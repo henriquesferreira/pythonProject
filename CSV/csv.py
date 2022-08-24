bb2459bcb3dfea436d00688ee81d678ba84439c6
henriquesferreira <107350138+henriquesferreira@users.noreply.github.com>
2022-08-24 16:07:04 +0100

    Atualização

---
+++ CSV/csv.py
@@ -0,0 +1,5 @@
+"""
+Comma Separated Values - CSV (Valores separados por virgula)
+É um formato de dados muito usado em tabelas (Excel, Google Sheets), bases de
+dados, clientes de e-mail, etc...
+"""
